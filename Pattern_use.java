516650b3a7a503bad17e6bab5e96880fb8ca4898
Rohit <rohitkushwaha7389@gmail.com>
2023-01-30 18:03:51 +0530

    Create Pattern_use.java

---
+++ Pattern_use.java
@@ -0,0 +1,21 @@
+public class Pattern_use {
+    public static void main(String []args){
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==4 || j==4){
+                    System.out.print("*");
+                }
+            
+                else{
+                    System.out.print(" ");
+                }
+            }
+        
+                System.out.println();
+            }
+        
+    }
+}
+    
+
