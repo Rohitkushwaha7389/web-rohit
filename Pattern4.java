7a6dfe9c6f02a7dd8539052e290d3edbf50d925b
Rohit <rohitkushwaha7389@gmail.com>
2023-02-02 00:47:33 +0530

    Create Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int i,j;
+        for(i=0;i<num;i++){
+            for(j=0;j<num;j++){
+                if(J==0 || i==0)
+            }
+        }
+    }
+    
+}
