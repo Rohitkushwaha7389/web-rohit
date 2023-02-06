024a521604e8ffb47a762bd3e735fcb6d08806da
Rohit <rohitkushwaha7389@gmail.com>
2023-02-06 14:45:19 +0530

    Create String_uses.java

---
+++ String_uses.java
@@ -0,0 +1,19 @@
+public class String_uses {
+    public static void main(String[] args) {
+        String s="Rohit";
+        System.out.println(s);
+        System.out.println("");
+
+        //  String r =new String("Rohit");
+        // System.out.println(s);
+        // s.concat("Kushwaha");
+        // System.out.println(s);
+        // char[] ch={'a','b','c'};
+        // String r=new String(ch);
+        // System.out.println(r);
+        byte[] b={100,101,102};
+        String str=new String(b);
+        System.out.println(str);
+    }
+    
+}
